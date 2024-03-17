fb7e454e29c6733410ab2634671831c1acbbcc90
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2024-03-17 21:34:21 +0100

    Add functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def console_input():
+    pass
+
+
+def file_input():
+    pass
+
+
+def file_input_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def console_write():
+    pass
+
+
+def file_write():
+    pass
+
+
+def file_write_pandas():
+    pass
